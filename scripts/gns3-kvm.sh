e0955192e76825c5c8ad38c35dc5cfe1a66f6629
mc-b <marcel.bernet@ch-open.ch>
2023-05-15 10:29:32 +0200

    WebVirtCloud

--- scripts/gns3-kvm.sh
+++ scripts/gns3-kvm.sh
@@ -7,6 +7,36 @@
 [ "${GNS3_SERVER}" == "" ] && { export GNS3_SERVER=localhost; }
 
 ### 
+# WebVirtCloud - einfaches UI fuer KVM
+
+export MODUL=webvirtcloud
+
+echo -e "instance-id: kvm-${MODUL}\nlocal-hostname: kvm-${MODUL}" > meta-data
+curl https://raw.githubusercontent.com/mc-b/lerngns3/main/scripts/cloud-init-webvirtcloud.yaml >user-data
+
+sudo mkisofs -output "/opt/gns3/images/QEMU/kvm-${MODUL}.iso" -volid cidata -joliet -rock {user-data,meta-data}
+sudo rm -f "/opt/gns3/images/QEMU/kvm-${MODUL}.iso.md5sum"
+    
+cat <<EOF >template
+{
+    "cdrom_image": "kvm-${MODUL}.iso",
+    "compute_id": "local",
+    "default_name_format": "{name}-{0}",
+    "console_type": "telnet",
+    "cpus": 1,
+    "ram": 2048,
+    "symbol": ":/symbols/affinity/circle/green/client.svg",    
+    "hda_disk_image": "jammy-server-cloudimg-amd64.img",
+    "name": "kvm-${MODUL}",
+    "qemu_path": "/bin/qemu-system-x86_64",
+    "template_type": "qemu",
+    "options": "--cpu host",
+    "usage": "KVM Host"
+}
+EOF
+curl -X POST "http://${GNS3_SERVER}:3080/v2/templates" -d "@template"
+
+### 
 # einfache KVM Umgebung
 
 export MODUL=host
@@ -25,7 +55,7 @@ cat <<EOF >template
     "console_type": "telnet",
     "cpus": 4,
     "ram": 8192,
-    "symbol": ":/symbols/affinity/circle/red/server.svg",    
+    "symbol": ":/symbols/affinity/circle/green/server.svg",    
     "hda_disk_image": "jammy-server-cloudimg-amd64.img",
     "name": "kvm-${MODUL}",
     "qemu_path": "/bin/qemu-system-x86_64",
