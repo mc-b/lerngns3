abb933830c42d69fe90c4366cb146884a21ed9e9
mc-b <marcel.bernet@ch-open.ch>
2023-05-15 13:43:49 +0200

    Unterschiedliche KVM Host wegen gleichen Hostnames

--- scripts/gns3-kvm.sh
+++ scripts/gns3-kvm.sh
@@ -41,27 +41,32 @@ curl -X POST "http://${GNS3_SERVER}:3080/v2/templates" -d "@template"
 
 export MODUL=host
 
-echo -e "instance-id: kvm-${MODUL}\nlocal-hostname: kvm-${MODUL}" > meta-data
-curl https://raw.githubusercontent.com/mc-b/lerngns3/main/scripts/cloud-init-kvm.yaml >user-data
+for INSTANCE in 01 02 03 
+do
 
-sudo mkisofs -output "/opt/gns3/images/QEMU/kvm-${MODUL}.iso" -volid cidata -joliet -rock {user-data,meta-data}
-sudo rm -f "/opt/gns3/images/QEMU/kvm-${MODUL}.iso.md5sum"
+    echo -e "instance-id: kvm-${MODUL}-${INSTANCE}\nlocal-hostname: kvm-${MODUL}-${INSTANCE}" > meta-data
+    curl https://raw.githubusercontent.com/mc-b/lerngns3/main/scripts/cloud-init-kvm.yaml >user-data
     
-cat <<EOF >template
+    sudo mkisofs -output "/opt/gns3/images/QEMU/kvm-${MODUL}-${INSTANCE}.iso" -volid cidata -joliet -rock {user-data,meta-data}
+    sudo rm -f "/opt/gns3/images/QEMU/kvm-${MODUL}-${INSTANCE}.iso.md5sum"
+        
+    cat <<EOF >template
 {
-    "cdrom_image": "kvm-${MODUL}.iso",
+    "cdrom_image": "kvm-${MODUL}-${INSTANCE}.iso",
     "compute_id": "local",
-    "default_name_format": "{name}-{0}",
+    "default_name_format": "{name}",
     "console_type": "telnet",
     "cpus": 4,
     "ram": 8192,
     "symbol": ":/symbols/affinity/circle/green/server.svg",    
     "hda_disk_image": "jammy-server-cloudimg-amd64.img",
-    "name": "kvm-${MODUL}",
+    "name": "kvm-${MODUL}-${INSTANCE}",
     "qemu_path": "/bin/qemu-system-x86_64",
     "template_type": "qemu",
     "options": "--cpu host",
-    "usage": "KVM Host"
+    "usage": "KVM Host - ${INSTANCE}"
 }
 EOF
-curl -X POST "http://${GNS3_SERVER}:3080/v2/templates" -d "@template"
+    curl -X POST "http://${GNS3_SERVER}:3080/v2/templates" -d "@template"
+
+done
