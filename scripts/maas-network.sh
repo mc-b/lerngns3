6d6d04cd948e34632fa7007d04870e3ffd92ecd8
mc-b <marcel.bernet@ch-open.ch>
2023-05-19 18:59:04 +0200

    addresses statt search

--- scripts/maas-network.sh
+++ scripts/maas-network.sh
@@ -10,10 +10,12 @@ network:
             addresses:
             - 192.168.123.8/24
             dhcp4: false
+            dhcp4-overrides:
+               use-dns: false
             dhcp6: false
             gateway4: 192.168.123.1
             nameservers:
-                search:
+                addresses:
                 - 208.67.222.222
                 - 208.67.220.22
     version: 2
